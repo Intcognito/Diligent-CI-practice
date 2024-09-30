cb05d5439464a1f83758d955f49da80ffea8a1e3
Intcognito <130778504+Intcognito@users.noreply.github.com>
2024-09-30 21:41:59 +0200

    implementing id validation for the complete command

--- src/validate.js
+++ src/validate.js
@@ -1,22 +1,25 @@
 import { AppError } from "./app-error.js";
 
 export function validateAddParams(params) {
-  if(params.length !== 1) {
+  if (params.length !== 1) {
     throw new AppError('Give a title as the only parameter in parenthesis.');
   }
   const [title] = params;
-  if(typeof title !== 'string' || title?.length === 0) {
+  if (typeof title !== 'string' || title?.length === 0) {
     throw new AppError('The title must be a non zero length string.')
   }
   return params;
 }
 
 export function validateId(id) {
-  if(typeof id !== 'number') {
+  const parsedId = parseInt(id[0]);
+
+  if (typeof parsedId !== 'number') {
     throw new AppError('The id must be a number.')
   }
-  if(id < 0) {
-    throw new AppError('The id must be a non-negative number.')
+  if (parsedId <= 0) {
+    throw new AppError('The id must be a number greater than zero.')
   }
-  return id;
+
+  return parsedId;
 }
