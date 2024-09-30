664de060ec892267c616b5be5863fdf3a9240725
Ati <nagyonkellvalamihez@gmail.com>
2024-09-30 11:55:15 +0200

    add validateFindByIdParams function

--- src/validate.js
+++ src/validate.js
@@ -1,12 +1,24 @@
 import { AppError } from "./app-error.js";
 
 export function validateAddParams(params) {
-  if(params.length !== 1) {
+  if (params.length !== 1) {
     throw new AppError('Give a title as the only parameter in parenthesis.');
   }
   const [title] = params;
-  if(typeof title !== 'string' || title?.length === 0) {
+  if (typeof title !== 'string' || title?.length === 0) {
     throw new AppError('The title must be a non zero length string.')
   }
   return params;
+}
+
+export function validateFindByIdParams(params) {
+  if (params.length !== 1) {
+    throw new AppError('You must provide exactly one ID.');
+  }
+
+  const [id] = params;
+  if (isNaN(id) || id <= 0) {
+    throw new AppError('The ID must be a numeric value greater than 0.');
+  }
+  return parseInt(params, 10);
 }
