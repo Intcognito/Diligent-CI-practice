a583476b075c67eefc62c3010da7b9af160c6ba9
zsolt270 <zsolt270000@gmail.com>
2024-10-01 10:53:31 +0200

    finished the edit title params validation

--- src/app.js
+++ src/app.js
@@ -1,34 +1,38 @@
-import { list, formatList, format, add, findById } from './todo.js';
-import { display } from './display.js';
-import { AppError } from './app-error.js';
-import { validateAddParams, validateFindByIdParams } from './validate.js';
+import { list, formatList, format, add, findById } from "./todo.js";
+import { display } from "./display.js";
+import { AppError } from "./app-error.js";
+import {
+	validateAddParams,
+	validateFindByIdParams,
+	validatedEditTitleParams,
+} from "./validate.js";
 
 export function createApp(todoStore, args) {
-  const [, , command, ...params] = args;
+	const [, , command, ...params] = args;
 
-  switch (command) {
-    case 'list':
-      const todos = list(todoStore)
-      display([
-        ...formatList(todos),
-        `You have ${todos.length} todos.`
-      ]);
-      break;
-    case 'add':
-      const validated = validateAddParams(params);
-      const added = add(todoStore, validated);
-      display(['New Todo added:', format(added)])
-      break;
-    case 'find-by-id':
-      const validatedId = validateFindByIdParams(params);
-      const todoById = findById(todoStore, validatedId);
-      if (!todoById) {
-        display([`Todo with ID ${validatedId} not found.`]);
-        break;
-      }
-      display(['Todo by Id:', format(todoById)]);
-      break;
-    default:
-      throw new AppError(`Unknown command: ${command}`)
-  }
+	switch (command) {
+		case "list":
+			const todos = list(todoStore);
+			display([...formatList(todos), `You have ${todos.length} todos.`]);
+			break;
+		case "add":
+			const validated = validateAddParams(params);
+			const added = add(todoStore, validated);
+			display(["New Todo added:", format(added)]);
+			break;
+		case "find-by-id":
+			const validatedId = validateFindByIdParams(params);
+			const todoById = findById(todoStore, validatedId);
+			if (!todoById) {
+				display([`Todo with ID ${validatedId} not found.`]);
+				break;
+			}
+			display(["Todo by Id:", format(todoById)]);
+			break;
+		case "edit-title":
+			const validatedParams = validatedEditTitleParams(params);
+			console.log(validateAddParams);
+		default:
+			throw new AppError(`Unknown command: ${command}`);
+	}
 }
--- src/validate.js
+++ src/validate.js
@@ -1,24 +1,43 @@
 import { AppError } from "./app-error.js";
 
 export function validateAddParams(params) {
-  if (params.length !== 1) {
-    throw new AppError('Give a title as the only parameter in parenthesis.');
-  }
-  const [title] = params;
-  if (typeof title !== 'string' || title?.length === 0) {
-    throw new AppError('The title must be a non zero length string.')
-  }
-  return params;
+	if (params.length !== 1) {
+		throw new AppError("Give a title as the only parameter in parenthesis.");
+	}
+	const [title] = params;
+	if (typeof title !== "string" || title?.length === 0) {
+		throw new AppError("The title must be a non zero length string.");
+	}
+	return params;
 }
 
 export function validateFindByIdParams(params) {
-  if (params.length !== 1) {
-    throw new AppError('You must provide exactly one ID.');
-  }
+	if (params.length !== 1) {
+		throw new AppError("You must provide exactly one ID.");
+	}
 
-  const [id] = params;
-  if (isNaN(id) || id <= 0) {
-    throw new AppError('The ID must be a numeric value greater than 0.');
-  }
-  return parseInt(params, 10);
+	const [id] = params;
+	if (isNaN(id) || id <= 0) {
+		throw new AppError("The ID must be a numeric value greater than 0.");
+	}
+	return parseInt(params, 10);
+}
+
+export function validatedEditTitleParams(params) {
+	if (params.length !== 2) {
+		throw new AppError("You must provide an ID and a new title as parameters");
+	}
+
+	const [id, newTitle] = params;
+
+	if (isNaN(id) || id <= 0) {
+		throw new AppError("The ID must be a numeric value greater than 0");
+	}
+	if (!isNaN(newTitle) || newTitle.length < 1) {
+		throw new AppError(
+			"The new title must be a string and has to be at least 1 character long"
+		);
+	}
+
+	return parseInt(id), newTitle;
 }
